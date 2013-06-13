3973fbef2dd2e8afd9e508967c5ce26e8d373b0a
Manuel Pastor <manuel.pastor@upf.edu>
2013-06-13 19:44:43 +0200

    ideas about how to customize models

--- src/version0000/imodel.py
+++ src/version0000/imodel.py
@@ -8,3 +8,12 @@ class imodel(model):
         self.numLV = 2
         self.pH = 7.4
 
+##        self.norma = True
+##        self.norma-standard = True
+##        self.norma-neutraliz = True
+##        self.norma-3D = True
+##
+##        self.MD = Pentacle
+##
+##        self.model = PLS
+##        self.model-autoscaling = False
