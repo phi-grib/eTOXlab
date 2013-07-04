18303022bac8bbb5092a12d8ea1f2a0eb0ac1f74
Manuel Pastor <manuel.pastor@upf.edu>
2013-07-04 18:02:10 +0200

    GNU 3 headers

--- src/tmpl-imodel.py
+++ src/tmpl-imodel.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
+##    Description    eTAM model template
+##                   
+##    Authors:       Manuel Pastor (manuel.pastor@upf.edu) 
+##
+##    Copyright 2013 Manuel Pastor
+##
+##    This file is part of eTAM.
+##
+##    eTAM is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 3.
+##
+##    eTAM is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with eTAM.  If not, see <http://www.gnu.org/licenses/>.
+
 from model import model
 
 class imodel(model):
